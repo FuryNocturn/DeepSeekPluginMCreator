35bfed5d859a1f868a7f79805110af6750c9f6ce
FuryNocturnTV (Tare Fernandez) <familia-fernandez@hotmail.es>
2025-06-13 02:07:52 +0200

    Carga de codigo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,20 @@
 rootProject.name = "DeepSeekPluginMCreator"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://mcreator.net/repo")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://mcreator.net/repo")
+    }
+}
+
+// Configuración para el plugin de ShadowJar
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+}
